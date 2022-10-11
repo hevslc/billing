566477c091a8c3b882fcde75fa40f72bc7068917
hevslc <hevelyn.sthefany@gmail.com>
2022-10-11 11:49:13 -0300

    start project

---
+++ test/billing_web/views/error_view_test.exs
@@ -0,0 +1,15 @@
+defmodule BillingWeb.ErrorViewTest do
+  use BillingWeb.ConnCase, async: true
+
+  # Bring render/3 and render_to_string/3 for testing custom views
+  import Phoenix.View
+
+  test "renders 404.json" do
+    assert render(BillingWeb.ErrorView, "404.json", []) == %{errors: %{detail: "Not Found"}}
+  end
+
+  test "renders 500.json" do
+    assert render(BillingWeb.ErrorView, "500.json", []) ==
+             %{errors: %{detail: "Internal Server Error"}}
+  end
+end
