9f35b9bd25a7c03ff39750b2572a6e7a2f1ae520
hevslc <hevelyn.sthefany@gmail.com>
2022-10-17 12:25:21 -0300

    fix: error with enum type

--- lib/billing_web/views/error_view.ex
+++ lib/billing_web/views/error_view.ex
@@ -27,8 +27,12 @@ defmodule BillingWeb.ErrorView do
   def translate_errors(%Changeset{} = changeset) do
     traverse_errors(changeset, fn {msg, opts} ->
       Enum.reduce(opts, msg, fn {key, value}, acc ->
-        String.replace(acc, "%{#{key}}", to_string(value))
+        String.replace(acc, "%{#{key}}", translate_value(value))
       end)
     end)
   end
+
+  defp translate_value({:parameterized, Ecto.Enum, _map}), do: ""
+
+  defp translate_value(value), do: to_string(value)
 end
